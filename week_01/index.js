31e0910311c2d17e540026e6d7e8f50b3aeba80f
mohd kaif <mk2818356@gmail.com>
2025-01-30 11:36:59 +0530

    finished second tastk

--- week_01/index.js
+++ week_01/index.js
@@ -4,4 +4,22 @@ const  input = "100xdevs"
 
 const output = crypto.createHash("sha256").update(input).digest("hex")
 
-console.log(output)
+// console.log(output)
+
+
+
+
+// fn() give us the number, who's hash start with 00000 zeros 
+function findHashWithPrefix(prefix){
+    let num = 0
+    while(true){
+        let inputStr = num.toString() // if want output with a selected input +  random number who's hash start with 00000 then (inp + num.toString()) 
+        const hash = crypto.createHash("sha256").update(inputStr).digest("hex")
+        if(hash.startsWith(prefix)){
+            return {input:inputStr , hash : hash}
+        }
+        num++
+    }
+}
+
+console.log(findHashWithPrefix("00000"))
